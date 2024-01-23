e16bf4ef4cc8d3df2f6555160a7a7a3a0a66c8d6
Sammy Trindade <samaratrindaderibeiro@gmail.com>
2024-01-23 17:14:39 -0300

    :zap: perf: add time of execution to print in terminal in main function

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from products import process_products_based_on_category
 from menu_utils import select_category_and_navigate
+import time
+
 
 def main():
     chosen_category = select_category_and_navigate()
     process_products_based_on_category(chosen_category)
+    
+
 
 if __name__ == "__main__":
+    initial_time  = time.time()
     main()
-    
+    end_time = time.time()
+    print(f"O Código levou {int(end_time - initial_time)} segundos para finalizar")
