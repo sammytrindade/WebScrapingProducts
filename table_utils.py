9e9312441dac6eb4ef1161ba058696334a7698a2
Sammy Trindade <samaratrindaderibeiro@gmail.com>
2024-01-23 11:17:05 -0300

    :sparkles: feat: creating a print_table function to print the table in the terminal

--- table_utils.py
+++ table_utils.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 import os
 
+
 def create_table(product_name, product_code, product_price, currency, product_link, date, screenshot_filename):
     return {
         'nome_produto_coleta': product_name,
@@ -10,3 +11,11 @@ def create_table(product_name, product_code, product_price, currency, product_li
         'data_coleta': date,
         'arquivo_do_preco': screenshot_filename
     }
+
+
+def print_table(data_df, chosen_category):
+    table_headers = data_df[0].keys()
+    data = tabulate(data_df, headers='keys', tablefmt='pretty',
+                    colalign=['left']*len(table_headers))
+    save_table_to_html(data_df, chosen_category)
+    print(data)
