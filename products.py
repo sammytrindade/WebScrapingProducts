2cec2029bf5dd6228c891f9d79f6a0a48a5a473d
Sammy Trindade <samaratrindaderibeiro@gmail.com>
2024-01-23 13:05:10 -0300

    :package: chore: adding necessary imports to products file

---
+++ products.py
@@ -0,0 +1,11 @@
+from webdriver_setup import webdriver_configurations
+from selenium.webdriver.common.by import By
+from time import sleep
+from pagination_utils import check_pagination, has_products, get_total_pages, navigate_to_page
+from table_utils import print_table
+from time import sleep
+from selenium.webdriver.common.by import By
+from table_utils import create_table
+from screenshot_utils import take_screenshot
+from datetime import datetime
+
