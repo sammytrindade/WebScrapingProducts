0a7087cfdfc60716d5321cd37170665d3452f0ac
Sammy Trindade <samaratrindaderibeiro@gmail.com>
2024-01-23 12:33:48 -0300

    :sparkles: feat: creating a take_screenshot function to create a new directory of screenshots and name this files

--- screenshot_utils.py
+++ screenshot_utils.py
@@ -8,3 +8,23 @@ def sanitize_filename(filename):
     filename = re.sub(r'[^a-zA-Z0-9_]', '', filename)
     return filename
 
+
+def take_screenshot(product_code, product_name, date):
+    try:
+        screenshot_dir = 'screenshots'
+        if not os.path.exists(screenshot_dir):
+            os.makedirs(screenshot_dir)
+
+        sanitized_product_name = sanitize_filename(product_name)
+
+        file_name = f"{product_code}_{sanitized_product_name}_{date}.png"
+        screenshot_path = os.path.join(screenshot_dir, file_name)
+
+        screenshot = pyautogui.screenshot()
+        screenshot.save(screenshot_path)
+
+        print(f"Screenshot salvo em: {screenshot_path}")
+        return file_name
+
+    except Exception as e:
+        print(f"Erro ao tirar screenshot: {e}")
